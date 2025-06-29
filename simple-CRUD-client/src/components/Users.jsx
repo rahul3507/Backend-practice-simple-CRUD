697a03233a1fd50316029d5089a3b55a6af07896
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-06-29 14:12:52 +0600

    Refactor user state management and enhance delete user feedback in Users component

--- simple-CRUD-client/src/components/Users.jsx
+++ simple-CRUD-client/src/components/Users.jsx
@@ -4,7 +4,8 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 export const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = React.useState(loadedUsers);
 
   const handleDeleteUser = (id) => {
     console.log("Deleting user with ID:", id);
@@ -14,11 +15,12 @@ export const Users = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.deletedCount > 0) {
-          alert("User deleted successfully!");
           // Optionally, you can refetch the users or update the state to remove the user from the list
           if (data.deletedCount > 0) {
             // Update the state or re-render the component with remainingUsers
             alert("User deleted successfully!");
+            const remainingUsers = users.filter((user) => user._id !== id);
+            setUsers(remainingUsers);
           }
         }
       })
