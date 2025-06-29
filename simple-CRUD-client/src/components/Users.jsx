f1a0f806b52114d09e89fc496ff2636b052766a6
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-06-29 10:12:01 +0600

    Implement user deletion functionality in the backend and update user display in the frontend

--- simple-CRUD-client/src/components/Users.jsx
+++ simple-CRUD-client/src/components/Users.jsx
@@ -5,13 +5,35 @@ import { useLoaderData } from "react-router-dom";
 
 export const Users = () => {
   const users = useLoaderData();
+
+  const handleDeleteUser = (id) => {
+    console.log("Deleting user with ID:", id);
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("User deleted successfully!");
+          // Optionally, you can refetch the users or update the state to remove the user from the list
+          if (data.deletedCount > 0) {
+            // Update the state or re-render the component with remainingUsers
+            alert("User deleted successfully!");
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting user:", error);
+      });
+  };
   return (
     <div>
       <h2> Total Users: {users.length} </h2>
       <ul>
         {users.map((user) => (
           <li key={user._id}>
-            {user.name} - {user.email}
+            {user._id} - {user.name} - {user.email}
+            <button onClick={() => handleDeleteUser(user._id)}>X</button>
           </li>
         ))}
       </ul>
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const cors = require("cors");
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -46,6 +46,16 @@ async function run() {
       const user = req.body;
       console.log("Received user:", user);
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+
+      console.log("Deleting user with ID:", id);
+
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
